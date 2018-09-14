2c458f19303e01463b034c2fee2e71a601b7f56a
Aaron <sturmfreelance@gmail.com>
2018-09-14 16:10:36 +0200

    Modelo ProductCategory

---
+++ src/Models/ProductCategoryModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace devuelving\core;
+
+use devuelving\core\ProductCategory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'product_category';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'product', 'category',
+    ];
+}
