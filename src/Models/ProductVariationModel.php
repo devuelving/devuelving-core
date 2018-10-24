8d92713c5079289cd84313f793cd5b86eb95cc58
David Cortés <dacoto@dacoto.com>
2018-10-24 15:27:33 +0200

    Update src/Models/ProductVariationModel.php

---
+++ src/Models/ProductVariationModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace devuelving\core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariationModel extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'product_variations';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'product', 'variation', 'price', 'related_image',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+}
