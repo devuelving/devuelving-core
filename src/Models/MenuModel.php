f2235fcf0b1cdb8cad0511060c2c37a742ef9e3a
David Cortés <dacoto@dacoto.com>
2018-11-02 13:17:11 +0100

    Update src/Models/MenuModel.php

--- src/Models/MenuModel.php
+++ src/Models/MenuModel.php
@@ -16,15 +16,14 @@ class MenuModel
         return  [
             ['type' => 0, 'text' => 'Inicio'],
             ['type' => 2, 'text' => 'Hogar', 'category' => 1],
-            ['type' => 1, 'text' => 'Perfumes y cosmética', 'category' => 2],
-            ['type' => 1, 'text' => 'Cuidado e higiene personal', 'category' => 3],
+            ['type' => 1, 'text' => 'Perfumes y Cosmética', 'category' => 2],
+            ['type' => 1, 'text' => 'Cuidado e Higiene', 'category' => 3],
             ['type' => 1, 'text' => 'Parafarmacia', 'category' => 4],
             ['type' => 1, 'text' => 'Automóvil', 'category' => 5],
-            ['type' => 1, 'text' => 'Nutrición sport', 'category' => 6],
-            ['type' => 2, 'text' => 'Dietética natural', 'category' => 7],
+            ['type' => 1, 'text' => 'Nutrición Sport', 'category' => 6],
+            ['type' => 2, 'text' => 'Dietética Natural', 'category' => 7],
             ['type' => 1, 'text' => 'Tecnología', 'category' => 8],
-            ['type' => 1, 'text' => 'Carne ecológica', 'category' => 181],
-            ['type' => 1, 'text' => 'Infantil', 'category' => 10]
+            ['type' => 1, 'text' => 'Carne Ecológica', 'category' => 181]
         ];
     }
 }
