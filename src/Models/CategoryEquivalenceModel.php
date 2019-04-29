fbbd04c0befe95f33b1b688de29fa20a0052cf40
Eduard Puigdemunt <puigdemunt@gmail.com>
2019-04-29 10:37:10 +0200

    Update CategoryEquivalenceModel.php
    remove softdeletes

--- src/Models/CategoryEquivalenceModel.php
+++ src/Models/CategoryEquivalenceModel.php
@@ -3,11 +3,9 @@
 namespace devuelving\core;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CategoryEquivalenceModel extends Model
 {
-    use SoftDeletes;
 
     /**
      * The table associated with the model.
