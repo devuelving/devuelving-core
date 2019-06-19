c62e95647944eb48308d1dc5ff1dfaa4a0dc0877
Eduard Puigdemunt <puigdemunt@gmail.com>
2019-06-19 11:02:19 +0200

    redireccionar les imatges al dominio propi

--- src/Models/ProductModel.php
+++ src/Models/ProductModel.php
@@ -228,10 +228,12 @@ class ProductModel extends Model
         $return = [];
         $images = DB::table('product_image')->where('product', $this->id)->orderBy('default', 'desc')->get();
         foreach ($images as $image) {
-            $return[] = config('app.cdn.url') . $image->image;
+            // $return[] = config('app.cdn.url') . $image->image;
+            $return[] = '/cdn/' . $image->image;
         }
         if (count($return) < 1) {
-            $return[] = config('app.cdn.url') . 'default.png';
+            // $return[] = config('app.cdn.url') . 'default.png';
+            $return[] = '/cdn/product/default.png';
         }
         return $return;
     }
